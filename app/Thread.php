5cf7a391286b6bf90591aae50654b508e2985093
Shane Poppleton <shane@bjja.com.au>
2018-08-09 15:32:59 +1000

    Episode 4 work

--- app/Thread.php
+++ app/Thread.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Thread extends Model
 {
+    protected $guarded = [];
     /**
      * @return string
      */
@@ -17,5 +18,15 @@ class Thread extends Model
     public function replies()
     {
         return $this->hasMany(Reply::class);
+    }
+
+    public function creator()
+    {
+        return $this->belongsTo('App\User', 'user_id');
+    }
+
+    public function addReply($reply)
+    {
+        $this->replies()->create($reply);
     }
 }
