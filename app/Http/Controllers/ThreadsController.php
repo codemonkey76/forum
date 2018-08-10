748835e8bc27e35a8cbd1490e2360e72ac2a2585
Shane Poppleton <shane@bjja.com.au>
2018-08-10 15:25:19 +1000

    Episode 11 work

--- app/Http/Controllers/ThreadsController.php
+++ app/Http/Controllers/ThreadsController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Channel;
 use App\Thread;
 use Illuminate\Http\Request;
 
@@ -21,9 +22,13 @@ class ThreadsController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Channel $channel)
     {
-        $threads = Thread::latest()->get();
+        if ($channel->exists) {
+            $threads = $channel->threads()->latest()->get();
+        } else {
+            $threads = Thread::latest()->get();
+        }
 
         return view('threads.index', compact('threads'));
     }
@@ -47,8 +52,8 @@ class ThreadsController extends Controller
     public function store(Request $request)
     {
         $this->validate($request, [
-            'title' => 'required',
-            'body' => 'required',
+            'title'      => 'required',
+            'body'       => 'required',
             'channel_id' => 'required|exists:channels,id',
         ]);
 
--- tests/Feature/ReadThreadsTest.php
+++ tests/Feature/ReadThreadsTest.php
@@ -22,14 +22,14 @@ class ReadThreadsTest extends TestCase
     public function a_user_can_view_all_threads()
     {
         $this->get('/threads')
-            ->assertSee($this->thread->title);
+             ->assertSee($this->thread->title);
     }
 
     /** @test */
     public function a_user_can_read_a_single_thread()
     {
         $this->get($this->thread->path())
-            ->assertSee($this->thread->title);
+             ->assertSee($this->thread->title);
     }
 
     /** @test */
@@ -38,6 +38,19 @@ class ReadThreadsTest extends TestCase
         $reply = factory('App\Reply')->create(['thread_id' => $this->thread->id]);
 
         $this->get($this->thread->path())
-            ->assertSee($reply->body);
+             ->assertSee($reply->body);
     }
+
+    /** @test */
+    function a_user_can_filter_threads_according_to_a_channel()
+    {
+        $channel            = create('App\Channel');
+        $threadInChannel    = create('App\Thread', ['channel_id' => $channel->id]);
+        $threadNotInChannel = create('App\Thread');
+
+        $this->get('/threads/' . $channel->slug)
+             ->assertSee($threadInChannel->title)
+             ->assertDontSee($threadNotInChannel->title);
+    }
+
 }
