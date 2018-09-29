3c189eef854666b43e11f42240519fb0c094853a
Shane Poppleton <shane@bjja.com.au>
2018-09-29 22:05:32 +1000

    Episode 82 work

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -15,7 +15,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp()
     {
         parent::setUp();
-
+        \DB::statement('PRAGMA foreign_keys=on');
         $this->disableExceptionHandling();
     }
 
